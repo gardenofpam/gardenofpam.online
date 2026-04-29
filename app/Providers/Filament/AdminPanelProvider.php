4613651dceb50d0237f786bdd82a7fc14e713e99
gardenofpam <gardenofpam@gmail.com>
2026-04-29 22:12:46 +0800

    make the cms panels better

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers\Filament;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Navigation\NavigationGroup;
 use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
@@ -32,6 +33,12 @@ class AdminPanelProvider extends PanelProvider
                 'primary' => Color::Violet,
             ])
             ->brandName('Paul Albert Mina')
+            ->navigationGroups([
+                NavigationGroup::make()->label('Inbox'),
+                NavigationGroup::make()->label('GardenOfPam CMS'),
+                NavigationGroup::make()->label('CPEMina CMS'),
+                NavigationGroup::make()->label('MinapaulData CMS'),
+            ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
